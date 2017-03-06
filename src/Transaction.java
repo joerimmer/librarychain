f370ed77bc9ea75a7dd8b8b7a964567eeec4b61d
joerimmer <joe@rimmertechpartners.com>
2017-03-06 11:33:50 -0600

    rsa algorithm

--- src/Transaction.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by joerimmer on 2017-03-04.
- */
-public class Transaction {
-}
---
+++ src/src/RSA.java
@@ -0,0 +1,60 @@
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by joerimmer on 2017-03-06.
+ */
+public class RSA {
+    BigInteger p, q, n, d, e;
+
+    public RSA(int bits)
+    {
+        Random rand = new Random();
+        p = new BigInteger(bits/2, 100, rand);
+        q = new BigInteger(bits/2, 100, rand);
+        n = p.multiply(q);
+
+        BigInteger totient = lcm(p.subtract(BigInteger.ONE), q.subtract(BigInteger.ONE));
+        e = new BigInteger("5");
+
+        while (totient.gcd(e).intValue() > 1)
+        {
+            e = e.add(new BigInteger("3"));
+        }
+
+        d = e.modInverse(totient);
+    }
+
+    public BigInteger encrypt(BigInteger message)
+    {
+        return message.modPow(e, n);
+    }
+
+    public BigInteger decrypt(BigInteger message)
+    {
+        return message.modPow(d, n);
+    }
+
+    public static BigInteger lcm(BigInteger a, BigInteger b)
+    {
+        return a.multiply(b.divide(a.gcd(b)));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        RSA rsa = new RSA(1024);
+
+        System.out.println("(e, d) = \n("+rsa.e.toString()+"\n, \n"+rsa.d.toString()+")");
+
+        while(true) {
+            System.out.println("Enter a string to be encrypted: ");
+            String str = scanner.nextLine();
+            BigInteger eStr = rsa.encrypt(new BigInteger(str.getBytes()));
+            String dStr = new String(rsa.decrypt(eStr).toByteArray());
+
+            System.out.println("Encrypted string: " + eStr);
+            System.out.println("Decrypted string: " + dStr);
+        }
+    }
+}
