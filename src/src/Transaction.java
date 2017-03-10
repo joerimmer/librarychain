1501a4603d13e16c247fd06777bd9c22c32b73f6
felix <felixbraun1@yahoo.ca>
2017-03-10 10:18:36 -0600

    Added Getters for time stamps and other informations

--- src/src/Transaction.java
+++ src/src/Transaction.java
@@ -1,5 +1,5 @@
 import java.time.LocalDateTime;
-
+import java.time.format.DateTimeFormatter;
 /**
  * Created by joerimmer on 2017-03-06.
  */
@@ -10,11 +10,29 @@ public class Transaction
     public LocalDateTime CheckedOutTimestamp;
     public LocalDateTime DueByTimestamp;
 
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     public Transaction(String BookID, String Borrower, LocalDateTime CheckedOutTimestamp, LocalDateTime DueByTimestamp)
     {
         this.BookID = BookID;
         this.Borrower = Borrower;
         this.CheckedOutTimestamp = CheckedOutTimestamp;
         this.DueByTimestamp = DueByTimestamp;
+    }
+
+    public String getBookID() {
+        return BookID;
+    }
+
+    public String getBorrower(){
+        return Borrower;
+    }
+
+    public String getCheckedOutTimestamp(){//Return the Checked out timestamp
+        return CheckedOutTimestamp.format(formatter); //convert to string and return
+    }
+
+    public String getDueByTimestamp() {//return due by timestamp
+        return DueByTimestamp.format(formatter); //convert to string and return
     }
 }
